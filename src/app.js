40c5881a4ae321ecd86fa702f752eef6d4978edd
Geovanna Lessa <geovannalessa2@gmail.com>
2022-12-08 23:14:40 -0300

    Criando uma rota com o metodo post e vizualizando com insomnia. Instalando Sqlite e Sqlite3. Criando uma database.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-const express = require('express');
+import {openDb} from './configDB.js';
+
+import express from 'express';
 const app = express();
+app.use(express.json());
+
+openDb();
 
 app.get('/', function(req,res){
-    res.send('oioi')
+    res.send('oioi');
 })
+app.post('/pessoa', function(req,res){
+    console.log(req.body);
+    res.json({
+        'statucCode': 200
+    })
+});
 
-
-app.listen(3000, ()=>{console.log('rodando..')})
+app.listen(3000, ()=>{console.log('rodando..')});
