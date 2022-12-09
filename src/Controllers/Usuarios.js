0db6b286ea3d8cd1efad8ff0789e0d13cd72e008
Geovanna Lessa <geovannalessa2@gmail.com>
2022-12-09 16:43:41 -0300

    Criada a tabela usuarios no banco de dados. Criado os controllers. Conseguindo adicionar usuarios com post.

---
+++ src/Controllers/Usuarios.js
@@ -0,0 +1,63 @@
+import { openDb } from "../configDB.js";
+
+export async function createTable() {
+    openDb().then(db => {
+        db.exec(
+            'CREATE TABLE IF NOT EXISTS Usuarios (id INTEGER PRIMARY KEY, nome TEXT, email TEXT, senha TEXT)'
+        )
+    })
+}
+
+//rotas get checar se é com usuarios ou usuario////selectUsuarios pq essa função irá mostrar todos os usuarios da tabela
+export async function selectUsuarios(req, res) {
+    openDb()
+        .then(db => {
+            db.all('SELECT * FROM Usuarios')
+                .then(usuarios => res)
+        })
+}
+//busca por id
+export async function selectUsuario(req, res) {
+    let id = req.body.id;
+    openDb()
+        .then(db => {
+            db.all('SELECT * FROM Usuarios WHERE id=?', [id])
+                .then(usuarios => res)
+        })
+}
+
+
+//InsertUsuario está no singular porque vai adicionar um Usuario na tabela Usuarios.
+export async function insertUsuario(usuarios) {
+    // let usuarios = req.body;
+    openDb()
+        .then(db => {
+            db.run('INSERT INTO Usuarios (nome, email, senha) VALUES (?,?,?)', [usuarios.nome, usuarios.email, usuarios.senha])
+        });
+    // res.json({
+    //     "statuscode": 200
+    // })
+}
+export async function updateUsuario(req, res) {
+    let usuarios = req.body;
+    openDb()
+        .then(db => {
+            db.exec('UPDATE Usuarios SET nome=?, email=?, senha=? WHERE id=?)', [usuarios.nome, usuarios.idade, usuarios.senha, usuarios.id])
+        });
+    res.json({
+        "statuscode": 200,
+        "msg":"adicionou um usuario"
+    })
+}
+
+export async function deleteUsuario(req, res) {
+    let usuarios = req.body;
+    openDb()
+        .then(db => {
+            db.all('DELETE * FROM Usuarios WHERE id=?', [id])
+                .then(res => res)
+        });
+    res.json({
+        "statuscode": 200
+    })
+}
--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-import {openDb} from './configDB.js';
+import { createTable, insertUsuario,updateUsuario,selectUsuarios,selectUsuario,deleteUsuario } from './Controllers/Usuarios.js';
 
 import express from 'express';
 const app = express();
 app.use(express.json());
 
-openDb();
-
-app.get('/', function(req,res){
-    res.send('oioi');
-})
-app.post('/pessoa', function(req,res){
-    console.log(req.body);
+app.post('/usuario', function(req,res){
+    insertUsuario(req.body)
     res.json({
-        'statucCode': 200
+        "statuscode":200
     })
-});
+})
+// import router from './routers/UsuariosRouters.js';
+// app.use(router);
 
 app.listen(3000, ()=>{console.log('rodando..')});
