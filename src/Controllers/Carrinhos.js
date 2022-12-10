2d35f4085e8f30c7f728c6df60fe5dd604252433
Geovanna Lessa <geovannalessa2@gmail.com>
2022-12-10 17:38:36 -0300

    Criado os metodos get, post, put, delete para a tabela Carrinhos

--- src/Controllers/Carrinhos.js
+++ src/Controllers/Carrinhos.js
@@ -7,10 +7,38 @@ export async function createTable() {
         )
     })
 }
+export async function selectCarrinhos() {
+    return openDb().then(db => {
+        return db.all(
+            'SELECT * FROM Carrinhos'
+        ).then(res=>res)
+    });
+}
+export async function selectCarrinho(id) {
+    return openDb().then(db => {
+        return db.get(
+            'SELECT * FROM Carrinhos WHERE id=?',[id]
+        ).then(res=>res)
+    });
+}
 export async function insertCarrinho(carrinhos) {
     openDb().then(db => {
         db.run(
             'INSERT INTO Carrinhos ( usuario_id, produto_id,status) VALUES (?,?,?)', [carrinhos.usuario_id,carrinhos.produto_id,carrinhos.status]
         )
+    });
+}
+export async function updateCarrinho(carrinhos) {
+    openDb().then(db => {
+        db.run(
+            'UPDATE Carrinhos SET usuario_id=?, produto_id=?,status=? WHERE id=?', [carrinhos.usuario_id,carrinhos.produto_id,carrinhos.status,carrinhos.id]
+        )
+    });
+}
+export async function deleteCarrinho(id) {
+    return openDb().then(db => {
+        return db.get(
+            'DELETE FROM Carrinhos WHERE id=?',[id]
+        ).then(res=>res)
     });
 }
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { createTable, insertCarrinho } from './Controllers/Carrinhos.js';
+import { createTable, insertCarrinho,updateCarrinho,selectCarrinhos,selectCarrinho,deleteCarrinho } from './Controllers/Carrinhos.js';
 import express from 'express';
 const app = express();
 app.use(express.json());
@@ -10,13 +10,35 @@ app.use(routerUsuarios);
 app.use(routerProdutos);
 
 createTable()
+app.get('/carrinhos', async(req,res)=>{
+    let carrinhos = await selectCarrinhos();
+    res.json(carrinhos)
+})
+app.get('/carrinho', async(req,res)=>{
+    let carrinho = await selectCarrinho(req.body.id);
+    res.json(carrinho)
+})
 app.post('/carrinho', (req,res)=>{
     insertCarrinho(req.body)
     res.json({
         "statuscode": 200
     })
-    console.log(req.body)
 })
-
-
+app.put('/carrinho', (req,res)=>{
+    if(req.body && !req.body.id){
+        res.json({
+            "statuscode": 400,
+            "msg": "Precisa informar um id"
+        })
+    }else{
+        updateCarrinho(req.body)
+        res.json({
+            "statuscode": 200
+        })
+    }
+})
+app.delete('/carrinho', async(req,res)=>{
+    let carrinho = await deleteCarrinho(req.body.id);
+    res.json(carrinho)
+})
 app.listen(3000, () => { console.log('rodando..') });
